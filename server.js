a3d8f5e8272ada02791a6e5e3046f38676c95a26
chikechris <cchike2017@gmail.com>
2019-09-12 15:51:38 -0400

    connected recipe route to cookBook route compnt

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 
+const cookBookRoute = require('./routes/cookBookRoute.js')
+
 const server = express()
 
 server.use(express.json())
+
+server.use('/api/recipes', cookBookRoute)
+
 
 server.use('/', (req, res) => {
   res.send(`<h2>Api Home</h2>`)
