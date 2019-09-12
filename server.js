f19ff81414a5c4b331a56119f81fdd7910d6957c
chikechris <cchike2017@gmail.com>
2019-09-12 15:40:53 -0400

    server running

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.use('/', (req, res) => {
+  res.send(`<h2>Api Home</h2>`)
+})
+
+module.exports = server
