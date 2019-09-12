ad4685ac6197ccb5f4696bc0f491d256ee833a62
chikechris <cchike2017@gmail.com>
2019-09-12 16:03:35 -0400

    added migration and seed files

--- knexfile.js
+++ knexfile.js
@@ -3,6 +3,7 @@
 module.exports = {
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
       filename: './data/cook-book.db3'
     },
@@ -11,6 +12,12 @@ module.exports = {
     },
     seeds: {
       directory: './data/seeds'
+    }
+  },
+  pool: {
+    afterCreate: (conn, done) => {
+      // runs after a connection is made to the sqlite engine
+      conn.run('PRAGMA foreign_keys = ON', done) 
     }
   }
 }
